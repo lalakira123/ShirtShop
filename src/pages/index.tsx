b43e74cef78977bc3e287b89ba4160905e70da20
Laerte <laerteakira@gmail.com>
2023-01-11 14:02:18 -0300

    feat: Home PAGE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import shirt1 from './../assets/shirts/1.svg'
+import shirt2 from './../assets/shirts/2.svg'
+import shirt3 from './../assets/shirts/3.svg'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
