2152839d6d1c6293bc882ee6834be40e612b56e1
Laerte <laerteakira@gmail.com>
2023-01-11 14:27:53 -0300

    feat: Keen Slider Carrousel at Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,22 @@ import shirt1 from './../assets/shirts/1.svg'
 import shirt2 from './../assets/shirts/2.svg'
 import shirt3 from './../assets/shirts/3.svg'
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
 export default function Home() {
+  const [slideRef] = useKeenSlider(
+    {
+      slides: {
+        perView: 3,
+        spacing: 48,
+      },
+    }
+  )
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={slideRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,8 +29,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
