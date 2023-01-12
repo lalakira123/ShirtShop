f2cb2e64dc9ba6ba268e99c56f705366347a67f9
Laerte <laerteakira@gmail.com>
2023-01-12 19:13:00 -0300

    feat: Product Page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
 	const { query } = useRouter()
 
 	return (
-		<h1>Product: { query.id }</h1>
+		<ProductContainer>
+			<ImageContainer>
+				
+			</ImageContainer>
+
+			<ProductDetails>
+				<h1>Camiseta X</h1>
+				<span>R$ 79,90</span>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sed aut asperiores dignissimos itaque ad dolore nostrum quos assumenda tempora nam blanditiis distinctio optio, enim veritatis autem eveniet architecto a.</p>
+
+				<button>
+					Comprar agora
+				</button>
+			</ProductDetails>
+		</ProductContainer>
 	)
 }
